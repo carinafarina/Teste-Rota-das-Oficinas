2442c49f98ae19070065c2d07b6dcd18958ac97b
Carina Farina <carinafarina2014@gmail.com>
2025-04-15 23:32:49 -0300

    Subindo as Models e controllers

---
+++ RO.DevTest.WebApi/Models/ItemVenda.cs
@@ -0,0 +1,11 @@
+public class ItemVenda
+{
+    public int Id { get; set; }
+    public int VendaId { get; set; }
+    public Venda Venda { get; set; }
+    public int ProdutoId { get; set; }
+    public Produto Produto { get; set; }
+    public int Quantidade { get; set; }
+    public decimal PrecoUnitario { get; set; }
+    public decimal Subtotal { get; set; }
+}
